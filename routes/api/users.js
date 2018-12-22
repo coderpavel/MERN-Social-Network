df6e2d86f23dd562597e22f747cf0ae505031e6b
Smart App Code <smartappcode@gmail.com>
2018-12-22 13:49:53 +0500

    Adding register route and following dependencies: bcryptjs and gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-        res.json({msg: "User Works"});
+const User = require('../../models/User');
+
+// @desc Register user
+// @access Public
+router.post('/register', (req, res) => {
+    User.findOne({
+        email: req.body.email
+    })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'email already exists' });
+            } else {
+
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // Size
+                    r: 'pg', // Rating
+                    d: 'rm' // Default
+                });
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar, // the same: avatar: avatar
+                    password: req.body.password
+                });
+
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+
+                    });
+                });
+            }
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
-
+/*
 const db = require('./config/keys').mongoURI;
 
 mongoose.connect(db, {
@@ -15,15 +15,27 @@ mongoose.connect(db, {
 }).then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
 
+*/
+
+mongoose.connect('mongodb://localhost/mern', {
+    useCreateIndex: true,
+    useNewUrlParser: true
+}).then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Could not connect to MongoDB'));
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 // Routes
-app.use('routes/api/users', users);
-app.use('routes/api/profile', profile);
-app.use('routes/api/posts', posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
